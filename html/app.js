b0cf85464d4ceb71767aa796289ca13597cc8d04
codisafish <trocheniefajnie@gmail.com>
2026-02-17 23:00:02 +0100

    another working version :)

--- html/app.js
+++ html/app.js
@@ -5,6 +5,8 @@ let draw_delta_label = document.getElementById("draw_delta")
 let b_won_btn = document.getElementById("b_won")
 let b_won_delta_label = document.getElementById("b_won_delta")
 
+let expected_score_lable = document.getElementById("expected_score")
+
 let a_elo_inpt = document.getElementById("a_elo")
 let b_elo_inpt = document.getElementById("b_elo")
 
@@ -13,6 +15,10 @@ let error_msg = document.getElementById("error_msg")
 a_won_btn.addEventListener("click", a_won)
 draw_btn.addEventListener("click", draw)
 b_won_btn.addEventListener("click", b_won)
+
+a_elo_inpt.addEventListener("input", calc_deltas)
+b_elo_inpt.addEventListener("input", calc_deltas)
+calc_deltas()
 
 async function game(K, a_elo, b_elo, score_a){
 	const response = await fetch("/api/game", {
@@ -44,11 +50,20 @@ async function game(K, a_elo, b_elo, score_a){
 	a_elo_inpt.value = a_elo_new
 	b_elo_inpt.value = b_elo_new
 	
-	calc_deltas(K, a_elo_new, b_elo_new)
+	calc_deltas()
 }
 
-function calc_deltas(K, a_elo, b_elo){
-	expected_score_a = 1 / (1 + (10 ** (b_elo - a_elo)))
+function calc_deltas(){
+	a_elo_raw = Number.isFinite(Number(a_elo_inpt.value))? Number(a_elo_inpt.value) : 0
+	b_elo_raw = Number.isFinite(Number(b_elo_inpt.value))? Number(b_elo_inpt.value) : 0
+	a_elo = Math.max(a_elo_raw, 10)
+	b_elo = Math.max(b_elo_raw , 10)
+	
+	k = 32
+
+	expected_score_a = 1 / (1 + (10 ** ((b_elo - a_elo) / 400)))
+	
+	expected_score_lable.textContent = expected_score_a
 	
 	a_won_a_delta = k * (1 - expected_score_a)
 	draw_a_delta = k * (0.5 - expected_score_a)
@@ -61,10 +76,11 @@ function calc_deltas(K, a_elo, b_elo){
 	draw_a_delta = Math.min(Math.max(draw_a_delta, a_delta_min), a_delta_max)
 	b_won_a_delta = Math.min(Math.max(b_won_a_delta, a_delta_min), a_delta_max)
 	
-	a_won_delta_label.textContent = ("a: " + a_won_a_delta + ", b: " + (-a_won_a_delta))
-	draw_delta_label.textContent = ("a: " + draw_a_delta + ", b: " + (-draw_a_delta))
-	b_won_delta_label.textContent = ("a: " + b_won_a_delta + ", b: " + (-b_won_a_delta))
 	
+	
+	a_won_delta_label.textContent = ("A: " + -(a_elo_raw - (a_elo + a_won_a_delta)).toFixed(2) + ", B: " + -(b_elo_raw - (b_elo - a_won_a_delta)).toFixed(2))
+	draw_delta_label.textContent = ("A: " + -(a_elo_raw - (a_elo + draw_a_delta)).toFixed(2) + ", B: " + -(b_elo_raw - (b_elo - draw_a_delta)).toFixed(2))
+	b_won_delta_label.textContent = ("A: " + -(a_elo_raw - (a_elo + b_won_a_delta)).toFixed(2) + ", B: " + -(b_elo_raw - (b_elo - b_won_a_delta)).toFixed(2))
 }
 
 function a_won(event){
